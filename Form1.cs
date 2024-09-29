855e2e76a30b6438668738223805f2fe2e46a3d2
Steven Chance <schance90588@uat.edu>
2024-09-29 15:51:01 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Assignment_3._1_Exception_Handling
+//Steven Chance
+//MS539
+//Assignment 3.1
+//Estimated Time: 1 hour
+//Actual Time: 4 hours
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //divide 100 by the input number
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            //parse the input from the textbox to an integer
+                int number = int.Parse(textBox1.Text);
+                int result = 100 / number;
+
+            //display the result of the operation
+                label1.Text = $"Result: {result}";
+            }
+            //catch the exception if the input is not a number
+            catch (FormatException)
+            {
+                label1.Text = "Please enter a valid number";
+            }
+
+            //catch the exception if the input is zero
+            catch (DivideByZeroException)
+            {
+                label1.Text = "Cannot divide by zero";
+            }
+        }
+        //open Form2
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Windows.Forms;
+
+namespace Assignment_3._1_Exception_Handling
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+        //showing my frustration having to do this assignment
+            InitializeComponent();
+            label1.Text = "THIS WAS A FRUSTRATING AND TOUGH ASSIGNMENT!";
+        }
+    }
+}
